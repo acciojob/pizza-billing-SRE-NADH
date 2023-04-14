c41db0186c0cfaee835d970574361c3ae327999c
SRE-NADH <sreenadh669@gmail.com>
2023-04-14 23:15:07 +0530

    first check

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,59 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+     private int vegPizza=300;
+      private  int nonvegPizza=400;
+     private boolean ischeeseadd;
+      private int cheeseprice=80;
+      private boolean addextratopping;
+      private int extratoppingveg=70;
+      private int getExtratoppingnonveg=120;
+      private boolean addtakeaway;
+      private int takeaway=20;
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        price=isVeg?vegPizza:nonvegPizza;
+        ischeeseadd =false;
+        addextratopping=false;
     }
 
     public int getPrice(){
+
         return this.price;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(ischeeseadd ==false){
+            ischeeseadd =true;
+            price+=cheeseprice;
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(addextratopping==false) {
+            price+= isVeg?extratoppingveg:getExtratoppingnonveg;
+            addextratopping=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(addtakeaway==false){
+            price+=takeaway;
+            addtakeaway=true;
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        String var="";
+        var+=isVeg?"Base Price Of The Pizza: "+vegPizza+"\n":"Base Price Of The Pizza: "+nonvegPizza+"\n";
+        if(ischeeseadd) var+="Extra Cheese Added: "+cheeseprice+"\n";
+        if(addextratopping){
+            var+=isVeg?"Extra Toppings Added: "+extratoppingveg+"\n":"Extra Toppings Added: "+getExtratoppingnonveg+"\n";
+        }
+        var+="Total Price: "+price+"\n";
+        this.bill=var;
         return this.bill;
     }
 }
